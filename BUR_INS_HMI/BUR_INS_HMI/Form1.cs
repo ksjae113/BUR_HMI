770849584032b7afe180c8c346b50344a43f0287
ksjae113 <ksjae113@naver.com>
2025-05-28 09:37:19 +0900

    BUR_INS_HMI

---
+++ BUR_INS_HMI/BUR_INS_HMI/Form1.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Data;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml;
+using System.Drawing;
+using ScottPlot;
+using System.Drawing.Interop;
+using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics.Eventing.Reader;
+
+namespace BUR_INS_HMI
+{
+    public partial class Form1 : Form
+    {
+        private Form3 f3;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitChart();
+
+        }
+
+        private void InitChart()
+        {
+            var plot = formsPlot1.Plot;
+            var rand = new Random();
+
+            int pointCount = 50;
+
+            for (int seriesIndex = 0; seriesIndex < 27; seriesIndex++)
+            {
+                double[] xs = ScottPlot.DataGen.Consecutive(pointCount);    //시간축 (50개)
+                double[] ys = new double[pointCount];
+
+                for (int i = 0; i < pointCount; i++)
+                {
+                    //    ys[i] = rand.NextDouble() * 100;    //각 y값은 0 ~100 사이의 난수
+                    //   ys[i] = 250 + rand.NextDouble(); 250~251
+               //    if (rand.NextDouble()>0.5)
+                    {
+                        ys[i] = 250 - (rand.NextDouble() * 10);
+                    }
+
+                }
+
+                plot.AddScatter(xs, ys, label: $"ROLL {seriesIndex + 1}");  // 각 시리즈(라벨) 범례에 추가
+            }
+
+            plot.SetAxisLimits(xMin: 0, xMax: 49, yMin: 200, yMax: 300);  //생략시 고정 범위 아니고 자동 범위로 바뀜.
+            //x축은 0~49사이만 표시 , y축 0~100 // 만약 사용자가 마우스로 줌/팬을 하게 하고 싶으면 윗 코드 생략하거나 조건부 실행 
+            plot.Legend(location: ScottPlot.Alignment.UpperRight);  //범례 위치 제어
+
+            // 축 라벨 추가
+            plot.XLabel("시간 (초)");
+            plot.YLabel("RPM");
+
+            //반영
+            formsPlot1.Refresh();
+        }
+
+        private void pic_stop_btn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("장비를 정지하시겠습니까?", "정지 확인", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+            }
+            else
+            {
+                // AppendLog($"프로그램 비상 정지\t#{+log_cnt}");
+                // log_cnt++;
+                MessageBox.Show("장비를 정지합니다.", "정지 명령");
+
+            }
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("종료하시겠습니까?", "종료", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+
+            f2.FormSendEvent += new Form2.FormSendDataHandler(DiseaseUpdateEventMethod);
+            //form2에 이벤트 추가
+
+            f2.ShowDialog();
+            //Show(), ShowDialog() 차이 
+            //Show: form2 호출후에도 form1 제어가능 , ShowDialog(): form2 호출후에는 form1 제어불가
+        }
+
+
+
+        private void logout_btn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("관리자 모드를 종료하시겠습니까?", "종료 확인", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("관리자 모드를 종료합니다.", "관리자 로그아웃");
+                sen_btn.Visible = false;
+                temp_btn.Visible = false;
+                ampare_btn.Visible = false;
+                record_btn.Visible = false;
+                logout_btn.Visible = false;
+                login_btn.Visible = true;
+            }
+        }
+
+        private void DiseaseUpdateEventMethod(object sender)
+        {
+            if ("TRUE".Equals(sender.ToString()))
+            {
+                temp_btn.Visible = true;
+                sen_btn.Visible = true;
+                ampare_btn.Visible = true;
+                record_btn.Visible = true;
+                login_btn.Visible = false;
+                logout_btn.Visible = true;
+            }
+
+        }
+
+        private void temp_btn_Click(object sender, EventArgs e)
+        {
+            if (f3 == null || f3.IsDisposed)  //Ver1
+            {
+                f3 = new Form3();
+                f3.FormSendEvent += new Form3.FormSendDataHandler(DiseaseUpdateEventMethod);
+            }
+
+            f3.Show();
+            f3.BringToFront();  //이미 열려있다면 앞으로
+
+            if (f3.temp_panel.Visible == true)
+                f3.ShowPanel(4);
+            else if (f3.temp_panel.Visible == false)
+                f3.ShowPanel(1);
+
+
+
+        }
+
+        private void sen_btn_Click(object sender, EventArgs e)
+        {
+
+            if (f3 == null || f3.IsDisposed)   //Ver1
+            {
+                f3 = new Form3();
+                f3.FormSendEvent += new Form3.FormSendDataHandler(DiseaseUpdateEventMethod);
+            };
+
+            f3.Show();
+            f3.BringToFront();  //이미 열려있다면 앞으로
+
+            if (f3.sensor_panel.Visible == true)
+                f3.ShowPanel(5);
+            else if (f3.sensor_panel.Visible == false)
+                f3.ShowPanel(2);
+
+        }
+
+        private void ampare_btn_Click(object sender, EventArgs e)
+        {
+            if (f3 == null || f3.IsDisposed)    //Ver1
+            {
+                f3 = new Form3();
+                f3.FormSendEvent += new Form3.FormSendDataHandler(DiseaseUpdateEventMethod);
+            }
+            f3.Show();
+            f3.BringToFront();  //이미 열려있다면 앞으로
+
+            if (f3.ampare_panel.Visible == true)
+                f3.ShowPanel(6);
+            else if (f3.ampare_panel.Visible == false)
+                f3.ShowPanel(3);
+
+
+        }
+
+        private void record_btn_Click(object sender, EventArgs e)
+        {
+           Form5 f5 = new Form5();
+
+            f5.Show();
+        }
+    }
+}
---
+++ BUR_INS_HMI/BUR_INS_HMI/Form3.cs
@@ -0,0 +1,156 @@
+﻿using ScottPlot.Drawing.Colormaps;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUR_INS_HMI
+{
+    public partial class Form3 : Form
+    {
+        public delegate void FormSendDataHandler(string s);
+        public event FormSendDataHandler FormSendEvent;
+        int column = 10;
+        decimal amp = 50;   //초기값
+        decimal err = 10;   //초기값
+
+        public Form3()
+        {
+            InitializeComponent();
+            amp_set.Text = amp.ToString("F1") + " mA";
+            err_set.Text = err.ToString("F1") + " %";
+            update_amp_err(amp, err);
+        }
+
+        public void ShowPanel(int panelIndex)
+        {
+
+
+               switch (panelIndex)       //이거 써도 별차이없는듯
+               {
+                   case 1: temp_panel.Visible = true;
+                       break;
+                   case 2: sensor_panel.Visible = true; 
+                       break;
+                   case 3: ampare_panel.Visible = true; 
+                       break;
+                   case 4: temp_panel.Visible = false;
+                       break;
+                   case 5 : sensor_panel.Visible = false;
+                       break;
+                   case 6 : ampare_panel.Visible = false;
+                       break;
+
+               }
+            
+        /*    switch (panelIndex)
+            {
+                case 1:
+                    {
+                        temp_panel.Visible = true;
+                        temp_lbl.Visible = true;
+                    }
+                    break;
+                case 2:
+                    {
+                        sensor_panel.Visible = true;
+                        sens_lbl.Visible = true;
+                    }
+                    break;
+
+                case 3:
+                    {
+                        ampare_panel.Visible = true;
+                        ampare_lbl.Visible = true;
+                    }
+                    break;
+                case 4:
+                    {
+                        temp_panel.Visible = false;
+                        temp_lbl.Visible = false;
+                    }
+                    break;
+                case 5:
+                    {
+                        sensor_panel.Visible = false;
+                        sensor_panel.Visible = false;
+                    }
+                    break;
+                case 6:
+                    {
+                        ampare_panel.Visible = false;
+                        ampare_lbl.Visible = false;
+                    }
+                    break;
+            }*/
+
+        }
+
+
+        private void update_amp_err(decimal amp, decimal err)   //목표전류, 정상전류 범위 계산 및 텍스트 업데이트
+        {
+            decimal[] channels = new decimal[] { 3, 2, 3, 3, 2, 3, 3, 2, 3, 3 };
+            Label[] trg_amp = new Label[] { target_amp1, target_amp2, target_amp3, target_amp4, target_amp5, target_amp6, target_amp7, target_amp8, target_amp9, target_amp10 };
+            Label[] err_ran = new Label[] { err_range1, err_range2, err_range3, err_range4, err_range5, err_range6, err_range7, err_range8, err_range9, err_range10 };
+
+            amp_set.Text = amp.ToString("F1") + " mA"; 
+            err_set.Text = err.ToString("F1") + " %";
+
+
+            for (int i = 0; i < column; i++)
+            {
+                decimal ampare = amp * channels[i];
+                decimal min = ampare * ((100.0M - err)/100.0M);
+                decimal max = ampare * ((100.0M + err) / 100.0M);
+                trg_amp[i].Text = ampare.ToString("F1") + " mA";
+                err_ran[i].Text = min.ToString("F1") + " ~ " + max.ToString("F1") + "\nmA";                
+            }
+        }
+
+
+
+        private void DiseaseUpdateEventMethod2(object sender, object unit)  //f4 입력에 맞춰 전류 업데이트
+        {
+            if ("mA".Equals(unit.ToString()))
+            {
+                amp = Convert.ToDecimal(sender.ToString());
+            }
+            else if ("%".Equals(unit.ToString()))
+            {
+                err = Convert.ToDecimal(sender.ToString());
+            }
+
+           if (amp != 0 || err != 0)
+            {
+                update_amp_err(amp, err);
+            }
+        }
+
+
+        private void amp_set_btn_Click(object sender, EventArgs e)
+        {
+            Form4 f4 = new Form4();
+            f4.unit_lbl.Text = "mA";
+            f4.FormSendEvent2 += new Form4.FormSendDataHandler2(DiseaseUpdateEventMethod2);
+            f4.Show();
+        }
+
+        private void err_set_btn_Click(object sender, EventArgs e)
+        {
+            Form4 f4 = new Form4();
+            f4.unit_lbl.Text = "%";
+            f4.FormSendEvent2 += new Form4.FormSendDataHandler2(DiseaseUpdateEventMethod2);
+            f4.Show();
+        }
+
+ 
+    }
+
+
+
+}
