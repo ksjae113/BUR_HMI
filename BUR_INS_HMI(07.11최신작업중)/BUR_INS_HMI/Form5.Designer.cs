b492e013a65fa7c099fe050693718d0c7a9c5e56
ksjae113 <ksjae113@naver.com>
2025-07-11 17:28:36 +0900

    25.07.11 작업후

---
+++ BUR_INS_HMI(07.11최신작업중)/BUR_INS_HMI/Form5.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BUR_INS_HMI
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 48F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(324, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(487, 86);
+            label1.TabIndex = 0;
+            label1.Text = "!! WARNING !!";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.warring;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(124, 109);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 72F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(160, 216);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 128);
+            label2.TabIndex = 2;
+            label2.Text = " ";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaptionText;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(207, 35);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(130, 117);
+            panel1.TabIndex = 3;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(984, 461);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form5";
+            Text = "CAUTION";
+            FormClosed += Form5_FormClosed;
+            Load += Form5_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Panel panel1;
+    }
+}
---
+++ BUR_INS_HMI(07.11최신작업중)/BUR_INS_HMI/Form5.cs
@@ -0,0 +1,68 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BUR_INS_HMI
+{
+    
+
+    public partial class Form5 : Form
+    {
+
+        private System.Windows.Forms.Timer blinkTimer;
+        private bool isFlashing = false;
+        private Form1 mainForm;
+
+        public Form5(Form1 parent, string message)
+        {
+            InitializeComponent();
+            this.mainForm = parent;
+
+            this.FormClosed += Form5_FormClosed;
+
+            blinkTimer = new System.Windows.Forms.Timer();
+            blinkTimer.Interval = 1000;
+            blinkTimer.Tick += (s, e) =>
+            {
+                if (message == "RED")
+                {
+                    this.BackColor = isFlashing ? Color.Black : Color.FromArgb(192, 0, 0);
+                    this.label2.Text = "구동 불량 발생";
+                    this.label2.ForeColor = isFlashing ? Color.Red : Color.Black;
+                }
+                else if (message == "YELLOW")
+                {
+                    this.BackColor = isFlashing ? Color.Black : Color.FromArgb(192, 0, 0);
+                    this.label2.Text = "구동 경고 발생";
+                    this.label2.ForeColor = isFlashing ? Color.Yellow : Color.Black;
+                }
+                isFlashing = !isFlashing;
+            };
+            blinkTimer.Start();
+        }
+
+
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            //   LoadDataFromDB();
+        }
+
+        private void Form5_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            blinkTimer?.Stop();
+            blinkTimer?.Dispose();
+            mainForm.get_message = false;
+        }
+    }
+
+}
