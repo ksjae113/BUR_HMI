1f45e023c061140ce684c02041538fbcb1af4e2b
ksjae113 <ksjae113@naver.com>
2025-07-30 17:26:04 +0900

    25.07.30 작업 후

--- BUR_INS_HMI(07.30)/BUR_INS_HMI/obj/Debug/net8.0-windows/BUR_INS_HMI.AssemblyInfo.cs
+++ BUR_INS_HMI(07.30)/BUR_INS_HMI/obj/Debug/net8.0-windows/BUR_INS_HMI.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("modbus_gpt")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BUR_INS_HMI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29010314e1a57ada52f0cfae8bc4a3180fff17a8")]
-[assembly: System.Reflection.AssemblyProductAttribute("modbus_gpt")]
-[assembly: System.Reflection.AssemblyTitleAttribute("modbus_gpt")]
+[assembly: System.Reflection.AssemblyProductAttribute("BUR_INS_HMI")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BUR_INS_HMI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
